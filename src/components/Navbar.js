8473797fc582629b79d4d5f366d5959f60e262a3
jessanetbloom <107596489+jessanetbloom@users.noreply.github.com>
2023-01-05 15:40:05 +0800

    header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from '../assets/Logo-1.webp';
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-      </ul>
-    </div>
+    <header className="sticky">
+      <div className="tob-bar">
+        <div className="main-content">
+          <div className="right">
+            <ul>
+              <li>
+                <a href="mailto:domains@netbloom.com"><FaEnvelope/> domains@netbloom.com</a>
+              </li>
+              <li>
+                <a href="tel:0273929428"><FaPhoneAlt/> 027 392 9428</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="main-bar">
+        <div className="main-content">
+          <div className="navbar">
+            <div className="nav-left">
+              <Link to="/"><img src={Logo} alt="Netbloom Logo" className="logo"/></Link>
+            </div>
+            <div className="nav-right">
+              <ul className="main-nav">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Websites</Link>
+                </li>
+                <li>
+                  <Link to="/blog">About</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Contact Us</Link>
+                </li>
+              </ul>
+              <button className="gradientButton1">Get a Quote</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import bannerBg from '../assets/images/digital-marketing-services-dunedin.webp';
+
+const banner = {
+  backgroundImage: `url(${bannerBg})`,
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+};
 
 export default function AllArticles() {
   return (
     <div className="Home">
-      <h1>Homepage</h1>
+      <div className="banner" style={banner}>
+        <div className="banner-overlay">
+          <div className="main-content">
+            <div className="banner-con">
+              <h1>Websites that Elevate your Organisation</h1>
+              <p>Reach Your Target Audience & Increase Your Enterprise Value With High-Impact SEO Services & Converting Web Design</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
