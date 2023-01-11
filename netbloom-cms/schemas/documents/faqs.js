af23f808553bfa9cfb4a6a9fe54488d1288b19bb
jessanetbloom <jessa@netbloom.co.nz>
2023-01-11 15:37:09 +0800

    rename folder

---
+++ netbloom-cms/schemas/documents/faqs.js
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity';
+
+export default defineType({
+  title: 'FAQs',
+  name: 'faqs',
+  type: 'document',
+  fields: [
+    defineField({
+      title: 'Question',
+      name: 'question',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Answer',
+      name: 'answer',
+      type: 'array', 
+      of: [{type: 'block'}]
+    }), 
+  ],
+});
